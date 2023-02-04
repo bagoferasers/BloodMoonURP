5de2548309523fafb0e686925e5ea7c80d9bead3
bagoferasers <ctrull60@gmail.com>
2023-02-04 11:36:01 -0600

    andagain
    andagain

--- Assets/Exit.cs
+++ Assets/Exit.cs
@@ -11,7 +11,7 @@ public class Exit : MonoBehaviour
     
     public IEnumerator goodbye( )
     {
-        yield return new WaitForSeconds( 2 );
+        yield return new WaitForSeconds( 3 );
         Debug.Log( "exitgame" );
         Application.Quit( );
     }
