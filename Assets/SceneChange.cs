380b21b40f7d45f2a90e5bf5ae5d6b82583d5829
bagoferasers <ctrull60@gmail.com>
2023-02-04 12:36:53 -0600

    n
    n

--- Assets/SceneChange.cs
+++ Assets/SceneChange.cs
@@ -39,9 +39,9 @@ public class SceneChange: MonoBehaviour
 
         while( canvasGroup.alpha < 1 )
         {
-            canvasGroup.alpha += Time.deltaTime * (float)0.5;
+            canvasGroup.alpha += Time.deltaTime * (float)0.3;
             //canvasGroupMenu.alpha -= Time.deltaTime / 2;
-            canvasGroupMenu.alpha -= Time.deltaTime * (float)0.5;
+            canvasGroupMenu.alpha -= Time.deltaTime * (float)0.3;
             yield return null;
         }
 
