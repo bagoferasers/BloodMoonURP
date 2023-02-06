0c9626101c299bdedf0cd455416e412864f80930
bagoferasers <ctrull60@gmail.com>
2023-02-05 22:59:18 -0600

    play button
    Play button now changes scene to village.

--- Assets/SceneChange.cs
+++ Assets/SceneChange.cs
@@ -9,24 +9,35 @@ public class SceneChange: MonoBehaviour
 { 
     public void mainMenu( )
     {
+        FadeMainOut( );
         StartCoroutine( ChangeToScene( "MainMenu" ) );
+    }
+
+    public void village( )
+    {
+        FadeMainOut( );
+        StartCoroutine( ChangeToScene( "Village" ) );
     }
 
     /* Changes scenes while implementing a fade-out for 3 seconds. */
     public IEnumerator ChangeToScene( string sceneToChangeTo )
     {
-        FadeMeOut( );
         yield return new WaitForSeconds( 3 );
         SceneManager.LoadScene( sceneToChangeTo );
     }
 
-    public void FadeMeOut( )
+    public void FadeMainOut( )
     {
-        StartCoroutine ( FadeOut( ) );
+        StartCoroutine( FadeOutMain( ) );
     }
 
-    /* Fades scene to black by decrementing alpha over time. */
-    IEnumerator FadeOut( )
+    public void FadeOut( )
+    {
+        StartCoroutine( FadeMeOut( ) );
+    }
+
+    /* Fades main scene to black by decrementing alpha over time. */
+    IEnumerator FadeOutMain( )
     {
         GameObject g = GameObject.Find( "darkyboi" );
         CanvasGroup canvasGroup = g.GetComponentInParent< CanvasGroup >( );
@@ -43,6 +54,22 @@ public class SceneChange: MonoBehaviour
         /* This makes sure buttons aren't interactable while fading out. */
         canvasGroup.interactable = false;
         canvasGroupMenu.interactable = false;
+        yield return null;
+    }
+
+    /* Fades scene to black by decrementing alpha over time. */
+    IEnumerator FadeMeOut( )
+    {
+        CanvasGroup canvasGroup = GetComponent< CanvasGroup >( );
+
+        while( canvasGroup.alpha > 0 )
+        {
+            canvasGroup.alpha -= Time.deltaTime / 2;
+            yield return null;
+        }
+
+        /* This makes sure buttons aren't interactable while fading out. */
+        canvasGroup.interactable = false;
         yield return null;
     }
 }
