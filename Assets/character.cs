b98ec324bd5e0bb318ff3e36bc6fbdfab44baace
bagoferasers <ctrull60@gmail.com>
2023-02-09 13:53:41 -0600

    buttons start
    buttons going left and right

--- Assets/character.cs
+++ Assets/character.cs
@@ -5,85 +5,31 @@ using UnityEngine;
 public class character : MonoBehaviour
 {
     [ Header( "Movement" ) ]
-    public float speed = 3f;
+    public float speed;
 
     Rigidbody2D rb2d;
-    public float forceMultiplier = 10;
-
-    //[Header("Temp Objects")]
-    //public MyBox myBox;
+    public float forceMultiplier;
 
     // Start is called before the first frame update
     void Start( )
     {
-        //GetComponent<SpriteRenderer>().color = Color.red;
         rb2d = GetComponent<Rigidbody2D>( );
-        //rb2d = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        //myBox = GameObject.FindGameObjectWithTag("Box").GetComponent<MyBox>();
     }
 
     // Update is called once per frame
     void Update( )
     {
-        //transform.position += new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"),0) * speed * Time.deltaTime;
-        rb2d.MovePosition( transform.position + ( new Vector3( Input.GetAxisRaw( "Horizontal" ),Input.GetAxisRaw( "Vertical" ) ) * Time.deltaTime * speed ) );
-
-        //rb2d.position += new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"),0) * speed * Time.deltaTime;
-        //amount of time since last frame
-        //Time.deltaTime;
-        ///if( Input.GetKeyDown( KeyCode.K ) )
-        ///{
-            //GetComponent<SpriteRenderer>().color = new Color(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f));
-            //sr.color = new Color(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f));
-            //myBox.ChangeColor(Color.black);
-            ///GameObject newBox = Instantiate( myBoxGamePrefab, transform.position, Quaternion.identity );
-        //}
-
-        ///if(Input.GetKeyDown(KeyCode.E))
-        ///{
-            ///if(myBox != null) 
-            ///
                 ///myBox.ChangeColor(Color.blac);
-            ///}
-        ///}
-    }
-
-
-    void FixedUpdate( )
-    {
+        if( Input.GetKey( KeyCode.RightArrow ) || Input.GetAxis("Horizontal") > 0 )
+        {
+            Debug.Log( "Moving right." );
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if( Input.GetAxis( "Horizontal" ) < 0 )
+        {
+            Debug.Log( "Moving left." );
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
         //rb2d.MovePosition( transform.position + ( new Vector3( Input.GetAxisRaw( "Horizontal" ),Input.GetAxisRaw( "Vertical" ) ) * Time.deltaTime * speed ) );
-        //rb2d.AddForce( new Vector2( Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")) * forceMultiplier * Time.deltaTime );
-        //rb2d.velocity = ( new Vector3( Input.GetAxisRaw( "Horizontal" ),Input.GetAxisRaw( "Vertical" ) ) * speed );
-
-    }     
-    /*   
-    private void OnTriggerEnter2D( Collider2D other )
-    {
-        Debug.Log( "We collided with a box!" );
-        other.GetComponent<AudioSource>().Play();
-        //other.GetComponent< SpriteRenderer >( );
-        //Destroy(other.GameObject);
-        if(other.GetComponent<MyBox>() == null)
-        {
-            myBox = other.GetComponent<MyBox>();
-        }
-        if(other.tag == "Food!")
-        {
-            Destroy(other.gameObject);
-            transform.localScale *= 1.1f;
-        }
-        
     }
-
-    private void OnTriggerExit2D( Collider2D other )
-    {
-        Debug.Log( "We exited a box!" );
-        //other.GetComponent< SpriteRenderer >( );
-        //Destroy(other.GameObject);
-        if(other.GetComponent<MyBox>() != null)
-        {
-            myBox = null;
-        }
-    }
-    */
 }
