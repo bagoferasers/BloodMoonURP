74d3dc675b7d971f8f98d4564a7747eb4c9d81e1
bagoferasers <ctrull60@gmail.com>
2023-02-10 17:59:28 -0600

    Pause Menu Update
    Now pause menu doesn't allow for interaction with game buttons.

--- Assets/character.cs
+++ Assets/character.cs
@@ -10,33 +10,44 @@ public class character : MonoBehaviour
     public float jumpForce;
 
     Rigidbody2D rb2d;
-    private bool moveLeft, moveRight, goJump;
+    private bool moveLeft, moveRight, goJump, arePaused;
+    GameObject menu;
+    GameObject pauseButton;
+    GameObject leftButton;
+    GameObject rightButton;
+    GameObject upButton;
     
     // Start is called before the first frame update
     void Start( )
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>( );
         moveLeft = false;
         moveRight = false;
         goJump = false;
+        arePaused = false;
+        menu = GameObject.Find( "Template" );
+        pauseButton = GameObject.Find( "PauseBlackButton" );
+        leftButton = GameObject.Find( "LeftBlackButton" );
+        rightButton = GameObject.Find( "RightBlackButton" );
+        upButton = GameObject.Find( "UpBlackButton" );
     }
 
     // Update is called once per frame
     void FixedUpdate( )
     {
         //rb2d.MovePosition( transform.position + ( new Vector3( Input.GetAxisRaw( "Horizontal" ),Input.GetAxisRaw( "Vertical" ) ) * Time.deltaTime * speed ) );
-        if( moveRight )
+        if( !arePaused && moveRight )
         {
             Debug.Log( "Moving right." );
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
-        else if( moveLeft )
+        else if( !arePaused && moveLeft )
         {
             Debug.Log( "Moving left." );
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
         
-        if( goJump )
+        if( !arePaused && goJump )
         {
             if( rb2d.velocity.y == 0 )
             {
@@ -71,15 +82,43 @@ public class character : MonoBehaviour
 
     public void onPause( )
     {
-        GameObject menu = GameObject.Find( "Template" );
-        CanvasGroup c = menu.GetComponent< CanvasGroup >( );
-        c.alpha = 1;
+        arePaused = true;
+        
+        // get canvas group from menu and button objects
+        CanvasGroup m = menu.GetComponent< CanvasGroup >( );
+        Button p = pauseButton.GetComponent<Button>( );
+        Button l = pauseButton.GetComponent<Button>( );
+        Button r = pauseButton.GetComponent<Button>( );
+        Button u = pauseButton.GetComponent<Button>( );
+
+        // show menu
+        m.alpha = 1;
+
+        // can't interact with game buttons while paused
+        p.interactable = false;
+        l.interactable = false;
+        r.interactable = false;
+        u.interactable = false;
     }
 
     public void onExitPause( )
     {
-        GameObject menu = GameObject.Find( "Template" );
-        CanvasGroup c = menu.GetComponent< CanvasGroup >( );
-        c.alpha = 0;
+        arePaused = false;
+        
+        // get canvas group from menu and button objects
+        CanvasGroup m = menu.GetComponent< CanvasGroup >( );
+        Button p = pauseButton.GetComponent<Button>( );
+        Button l = pauseButton.GetComponent<Button>( );
+        Button r = pauseButton.GetComponent<Button>( );
+        Button u = pauseButton.GetComponent<Button>( );
+
+        // show menu
+        m.alpha = 0;
+
+        // can't interact with game buttons while paused
+        p.interactable = true;
+        l.interactable = true;
+        r.interactable = true;
+        u.interactable = true;
     }
 }
