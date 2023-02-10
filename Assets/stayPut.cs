f825c82aba1821d226f8f35b303635b4468da66e
bagoferasers <ctrull60@gmail.com>
2023-02-10 11:19:13 -0600

    changed button size
    size and also created another "folder"

--- Assets/stayPut.cs
+++ Assets/stayPut.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class stayPut : MonoBehaviour
 {
-    private GameObject g;
+    public GameObject g;
     private Vector3 point;
     
     // Start is called before the first frame update
     void Start()
     {
-        g = GameObject.Find("Player");
+        point = new Vector3( g.transform.position.x, transform.position.y, g.transform.position.z );
+        transform.position = Vector3.MoveTowards( transform.position, point, 0 );
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         point = new Vector3( g.transform.position.x, transform.position.y, g.transform.position.z );
         transform.position = Vector3.MoveTowards( transform.position, point, 0 );
