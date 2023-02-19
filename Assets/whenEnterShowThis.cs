572eb8d634e39a6519f30749116cb87d02481207
bagoferasers <ctrull60@gmail.com>
2023-02-19 11:15:05 -0600

    c
    c

---
+++ Assets/whenEnterShowThis.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class whenEnterShowThis : MonoBehaviour
+{
+    public TextMeshProUGUI showText;
+    public string textToShow;
+    public string nameOfSceneToChangeTo;
+    public Button aButton;
+    public GameObject objectToInteractWith;
+
+    void Start( )
+    {
+        showText.text = "";
+        aButton.onClick.AddListener( Interact );
+    }    
+
+    void Update( )
+    {
+        if( objectToInteractWith != null && Input.GetKeyDown( 0 ) )
+        {
+            Interact( );
+        }
+    }    
+
+    public void goToScene( )
+    {
+        SceneManager.LoadScene( nameOfSceneToChangeTo );
+    }
+
+    void OnTriggerEnter( Collider thisCollider )
+    {
+        if( thisCollider.gameObject.CompareTag( "showMe" ) )
+        {
+            showText.text = textToShow;
+            aButton.gameObject.SetActive( true );
+        }
+    }
+
+    void OnTriggerExit( Collider thisCollider )
+    {
+        if( thisCollider.gameObject.CompareTag( "showMe" ) )
+        {
+            showText.text = "";
+            objectToInteractWith.gameObject.SetActive( false );
+        }
+    }
+
+    void Interact( )
+    {
+        Debug.Log( "Changing scene" );
+        goToScene( );
+    }
+}
